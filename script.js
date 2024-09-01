970c7365c74ddf4af4852a6cf36abb2099ccf260
Dark-prince <barathraj0504@gmail.com>
2024-09-01 10:23:21 +0530

    Add resize button which asks input from the user and create a new sketch, Add clear button that clears the current sketch

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const resizeBtn = document.querySelector("#resizeBtn");
 
 let createDivs = (num) => {
   let boxSize = Math.floor(500 / num);
@@ -19,4 +20,25 @@ let createDivs = (num) => {
   container.setAttribute("style", "display:flex; flex-wrap:wrap;");
 };
 
-createDivs(16);
+let removeDivs = () => {
+  let boxes = Array.from(container.children);
+  boxes.forEach((element) => {
+    container.removeChild(element);
+  });
+};
+let size = 16;
+
+const clearBtn = document.querySelector("#clearBtn");
+clearBtn.addEventListener("click", () => {
+  removeDivs();
+  createDivs(size);
+});
+
+resizeBtn.addEventListener("click", () => {
+  do {
+    size = +prompt("Enter a number between 1-100");
+  } while (size > 100 || size < 1);
+  removeDivs();
+  createDivs(size);
+});
+createDivs(size);
